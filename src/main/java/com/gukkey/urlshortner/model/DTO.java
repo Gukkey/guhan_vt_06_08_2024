a8f1499b32ffbf410a8cb9cbfc3c72cf51df256a
Guhan <74915094+Gukkey@users.noreply.github.com>
2024-08-05 22:31:38 +0530

    chore(model): added allargs and noargs constructors

--- src/main/java/com/gukkey/urlshortner/model/DTO.java
+++ src/main/java/com/gukkey/urlshortner/model/DTO.java
@@ -1,12 +1,16 @@
 package com.gukkey.urlshortner.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DTO {
     protected String destinationURL;
     protected long days;
